388a7144c56e4b5e406d8592acd4aae46efc5c26
wowr00d <declanevo@gmail.com>
2016-09-06 17:05:19 +1000

    Added function for opening csv and returning variables

--- assignment.py
+++ assignment.py
@@ -2,22 +2,18 @@ import csv  # imports the csv module
 
 
 def main():  # main function that controls entire program
-    print('{:>15}'.format('centered'))
     valid_choices = ["R", "C", "A", "M", "Q"]
-    items = open('items.csv')
-    csv_items = csv.reader(items)
-    items_list = list(csv_items)
-    print(items_list)
+    items_list = open_csv()
 
-    user_choice = start_menu(csv_items, items)  # calls the startup menu and checks what the user wants
+    user_choice = start_menu(items_list)  # calls the startup menu and checks what the user wants
 
     while user_choice != "Q":
 
         if user_choice == "R":
-                required_items(items_list)  # calls the required items function
+            required_items(items_list)  # calls the required items function
 
         elif user_choice == "C":
-                completed_items(items_list)  # calls the completed items function
+            completed_items(items_list)  # calls the completed items function
 
         else:
             print("Incorrect Input")
@@ -26,10 +22,19 @@ def main():  # main function that controls entire program
 
     print("USER CHOSE Q")
 
-def start_menu(cvs_items, items):  # function for the startup menu
+
+def open_csv():
+    items = open('items.csv')
+    csv_items = csv.reader(items)
+    opened_list = list(csv_items)
+    sum_items = len(items.readlines())
+    return opened_list, items, sum_items
+
+
+def start_menu(items_list):  # function for the startup menu
     print("Shopping List 1.0 - By Declan Evanson")
-    total_csv_items = len(items.readlines())
-    print("Successfully loaded {} items from {} \n".format(total_csv_items, items.name))
+    total_csv_items = items_list[2]
+    print("Successfully loaded {} items from {} \n".format(total_csv_items, items_list[1].name))
     print(
         "Menu: \n" + "R - List Required Items \n" + "C - List Completed Items \n" + "A - Add New Items \n" + "M - Mark as completed \n" + "Q - Quit \n")
     choice = (input("Enter your choice: "))
@@ -38,7 +43,8 @@ def start_menu(cvs_items, items):  # function for the startup menu
 
 
 def print_menu():  # function for the menu that is displayed after initial startup
-    print("Menu: \n" + "R - List Required Items \n" + "C - List Completed Items \n" + "A - Add New Items \n" + "M - Mark as completed \n" + "Q - Quit \n")
+    print(
+        "Menu: \n" + "R - List Required Items \n" + "C - List Completed Items \n" + "A - Add New Items \n" + "M - Mark as completed \n" + "Q - Quit \n")
     choice = input("Enter your choice: ")
     choice = choice.upper()
     return choice
@@ -48,7 +54,7 @@ def required_items(items_list):  # function for the check required items option
     print("List required Items: \n")
     price_totals = 0
     row_total = 0
-    for row in items_list:
+    for row in items_list[0]:
         if "r" in row:
             row[1] = float(row[1])
             row[2] = int(row[2])
@@ -62,11 +68,12 @@ def required_items(items_list):  # function for the check required items option
 
     return
 
+
 def completed_items(items_list):  # function for the check completed items options
     print("List completed Items: \n")
     price_totals = 0
     row_total = 0
-    for row in items_list:
+    for row in items_list[0]:
         if "c" in row:
             row[1] = float(row[1])
             row[2] = int(row[2])
@@ -76,7 +83,7 @@ def completed_items(items_list):  # function for the check completed items optio
     if price_totals == 0:  # a check to see if there are actually any completed items
         print("No completed items")
     else:
-        print("Total price of {} item/s is $ {}".format(row_total, price_totals))
+        print("Total price of {} item/s is $ {} \n".format(row_total, price_totals))
 
     return
 
