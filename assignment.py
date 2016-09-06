f49992a1b02e9980950226c8f46f6b5bff2f3911
wowr00d <declanevo@gmail.com>
2016-09-06 16:19:37 +1000

    Added loops and fixed printmenu error that would just end program after a choice was entered

--- assignment.py
+++ assignment.py
@@ -2,6 +2,7 @@ import csv  # imports the csv module
 
 
 def main():  # main function that controls entire program
+    print('{:>15}'.format('centered'))
     valid_choices = ["R", "C", "A", "M", "Q"]
     items = open('items.csv')
     csv_items = csv.reader(items)
@@ -10,16 +11,20 @@ def main():  # main function that controls entire program
 
     user_choice = start_menu(csv_items, items)  # calls the startup menu and checks what the user wants
 
-    while user_choice not in valid_choices:
-        print("Incorrect Input")
-        user_choice = print_menu()  # calls the menu again after an incorrect input
+    while user_choice != "Q":
 
-    if user_choice == "R":
-        required_items(items_list)  # calls the required items function
+        if user_choice == "R":
+                required_items(items_list)  # calls the required items function
 
-    if user_choice == "C":
-        completed_items(items_list)  # calls the completed items function
+        elif user_choice == "C":
+                completed_items(items_list)  # calls the completed items function
 
+        else:
+            print("Incorrect Input")
+
+        user_choice = print_menu()
+
+    print("USER CHOSE Q")
 
 def start_menu(cvs_items, items):  # function for the startup menu
     print("Shopping List 1.0 - By Declan Evanson")
@@ -47,15 +52,15 @@ def required_items(items_list):  # function for the check required items option
         if "r" in row:
             row[1] = float(row[1])
             row[2] = int(row[2])
-            print(row_total, row[0], "$ ", row[1], row[2])
+            print("{} {} ${} {}".format(row_total, row[0], row[1], row[2]))
             price_totals += row[1]
             row_total += 1
     if price_totals == 0:  # a check to see if there are actually any required items
         print("No required items")
     else:
-        print("Total price of {} item/s is $ {}".format(row_total, price_totals))
+        print("Total price of {} item/s is ${} \n".format(row_total, price_totals))
 
-    print_menu()
+    return
 
 def completed_items(items_list):  # function for the check completed items options
     print("List completed Items: \n")
@@ -73,7 +78,7 @@ def completed_items(items_list):  # function for the check completed items optio
     else:
         print("Total price of {} item/s is $ {}".format(row_total, price_totals))
 
-    print_menu()
+    return
 
 
 main()
